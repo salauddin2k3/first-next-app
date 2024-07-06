c959b4853836c5a0218f2534bb300784e2e23d10
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2024-07-06 20:28:54 +0600

    Test: Route working or not

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet amet consectetur vero dolores sint velit molestias facilis nisi at odio mollitia labore ipsam minima dolorum perferendis sunt delectus blanditiis. Quam enim accusamus corporis modi, doloremque unde ullam cupiditate, quasi placeat aliquam ea? Dignissimos esse nihil incidunt repellendus odio assumenda harum earum doloribus id eius. Iste rerum itaque dolores necessitatibus repudiandae praesentium delectus recusandae facilis nobis provident voluptatem impedit eligendi neque, veniam consequuntur labore aliquam aut voluptatibus vero unde cumque veritatis, tempora vel. Assumenda, qui cumque nemo vero sapiente non sequi natus molestias expedita voluptates obcaecati quo quaerat in consequatur nulla doloribus est nihil itaque debitis. Praesentium quam porro alias recusandae vero? Placeat sequi inventore, soluta quod eos alias praesentium repellendus incidunt consectetur porro ut officiis magni facilis expedita libero similique? Quas alias et eos facere autem. Recusandae, praesentium. Deserunt totam pariatur tempore velit quibusdam dolor repellendus facere officiis vero quis delectus quasi fugit assumenda, voluptatem corrupti dolore a atque corporis, aliquam voluptas iusto esse maxime. Nobis, voluptates! Illum ipsam animi laboriosam neque fuga cum ad nihil? Dicta facilis aliquam maiores et quae, repellendus sapiente distinctio explicabo quaerat rem aperiam omnis assumenda, sint vero obcaecati natus a est at molestiae.</p>
+      </div>
+    </main>
+  );
+}
